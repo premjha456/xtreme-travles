2b21be3f891ead7df8b79a13baf04287713d1be1
Prem <Prem@Prem-PC>
2018-04-02 02:19:10 +0530

    Bus seat Select

--- xtremetravles/src/main/webapp/assets/js/script.js
+++ xtremetravles/src/main/webapp/assets/js/script.js
@@ -597,5 +597,53 @@ var $tabel = $('#cabTable');
 				
 				} 	
 
+			
+			
+		    
+		    $(function () {
+		            var settings = {
+		            rows: 4,
+		                cols: 10,
+		                rowCssPrefix: 'row-',
+		                colCssPrefix: 'col-',
+		                seatWidth: 40,
+		                seatHeight: 40,
+		                seatCss: 'seat',
+		                selectedSeatCss: 'selectedSeat',
+		        selectingSeatCss: 'selectingSeat'
+		            };
+		            var init = function (reservedSeat) {
+		                var str = [], seatNo, className;
+		                for (i = 0; i < settings.rows; i++) {
+		                    for (j = 0; j < settings.cols; j++) {
+		                        seatNo = (i + j * settings.rows + 1);
+		                        className = settings.seatCss + ' ' + settings.rowCssPrefix + i.toString() + ' ' + settings.colCssPrefix + j.toString();
+		                        if ($.isArray(reservedSeat) && $.inArray(seatNo, reservedSeat) != -1) {
+		                            className += ' ' + settings.selectedSeatCss;
+		                        }
+		                        str.push('<li class="' + className + '"' +
+		                                  'style="top:' + (i * settings.seatHeight).toString() + 'px;left:' + (j * settings.seatWidth).toString() + 'px">' +
+		                                  '<a title="' + seatNo + '">' + seatNo + '</a>' +
+		                                  '</li>');
+		                    }
+		                }
+		                $('#place').html(str.join(''));
+		            };
+		            //case I: Show from starting
+		            //init();
+		            //Case II: If already booked
+		            var bookedSeats = [5, 10, 25,35,45,55];
+		            init(bookedSeats);
+		            $('.' + settings.seatCss).click(function () {
+		      if ($(this).hasClass(settings.selectedSeatCss)){
+		        alert('This seat is already reserved');
+		      }
+		      else{
+		                $(this).toggleClass(settings.selectingSeatCss);
+		        }
+		            });
+		            
+		        });
+
 		
 });
